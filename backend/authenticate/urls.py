07a628807be499b06fad2290820cd6063dfff881
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2025-11-08 09:09:49 +0100

    proper register url

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -5,4 +5,5 @@ import api.urls
 urlpatterns = [
     path("login", response.login, name="login"),
     path("logout", response.logout, name="logout"),
+    path("register", response.register, name="register"),
 ]
