7648e36e329fbe762ae9781f93adcfb6956541b4
zsuczko <instabb6464@gmail.com>
2025-11-09 09:58:21 +0100

    player cards

---
+++ frontend/app/routes/app.master.game._index/route.tsx
@@ -0,0 +1,5 @@
+import MasterGameCreator from "./masterGameCreator";
+
+export default function MasterGamefield() {
+  return <MasterGameCreator></MasterGameCreator>;
+}
---
+++ frontend/app/routes/app.master.game.player/route.tsx
@@ -0,0 +1,5 @@
+import PlayerDeck from "./playerDeck";
+
+export default function MasterCollection() {
+  return <PlayerDeck></PlayerDeck>;
+}
