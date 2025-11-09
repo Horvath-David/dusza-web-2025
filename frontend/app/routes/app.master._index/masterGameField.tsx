65f658cd4ff561fc4b631221f01fca361e7b1ded
zsuczko <instabb6464@gmail.com>
2025-11-09 14:35:39 +0100

    Create and delete world

--- frontend/app/routes/app.master._index/masterGameField.tsx
+++ frontend/app/routes/app.master._index/masterGameField.tsx
@@ -1,4 +1,4 @@
-import { Plus } from "lucide-react";
+import { FileStack, Pencil, Plus, Swords, Trash } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { toast } from "sonner";
@@ -9,10 +9,25 @@ import {
   DialogHeader,
   DialogTrigger,
 } from "~/components/ui/dialog";
-import { Field, FieldGroup, FieldLabel, FieldSet } from "~/components/ui/field";
+import { Field, FieldLabel, FieldSet } from "~/components/ui/field";
 import { Input } from "~/components/ui/input";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemHeader,
+} from "~/components/ui/item";
+import { Separator } from "~/components/ui/separator";
 import { API_URL } from "~/constants";
 import { MasterGeneralContext } from "~/context/MasterGeneralContext";
+
+type WorldType = {
+  id: number;
+  name: string;
+  cards: number;
+  dungeons: number;
+};
 
 const MasterGameField = () => {
   const navi = useNavigate();
@@ -20,6 +35,8 @@ const MasterGameField = () => {
   const [gameName, setGameName] = useState<string>();
 
   const { worldId, setWorldId } = useContext(MasterGeneralContext);
+
+  const [allWorld, setAllWorld] = useState<WorldType[]>([]);
 
   useEffect(() => {
     getWorlds();
@@ -32,7 +49,31 @@ const MasterGameField = () => {
     });
 
     const data = await response.json();
-    console.log(data);
+
+    data.worlds.forEach((element: WorldType) => {
+      setAllWorld((prev) => [...prev, element]);
+    });
+    // console.log(data);
+  };
+
+  const OnModifyDungeon = (id: number) => {
+    setWorldId(id);
+    navi("game");
+  };
+
+  const OnWorldDelete = async (id: number) => {
+    const response = await fetch(API_URL + `/world/${id}/delete`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    if (!response.ok) {
+      toast.error("Valami nem sikerült");
+      return;
+    }
+
+    setAllWorld(allWorld.filter((e) => e.id !== id));
+    toast.success("Sikeres törlés");
+    return;
   };
 
   const OnSubmits = async () => {
@@ -69,7 +110,49 @@ const MasterGameField = () => {
         <h1 className="font-medium mt-8 py-2 text-xl bg-linear-to-b from-black via-black to-neutral-500 dark:from-white via-50% dark:via-white dark:to-neutral-600 bg-clip-text text-transparent">
           Játékaid
         </h1>
-        <div className="border-2 border-gray-500 rounded-2xl min-w-[50em] h-[20em]"></div>
+        <div className="border-2 border-gray-500 rounded-2xl min-w-[50em] h-[20em] overflow-auto">
+          {allWorld.map((e) => {
+            return (
+              <>
+                <Item>
+                  <ItemContent>
+                    <ItemHeader className="font-bold text-xl">
+                      {e.name}
+                    </ItemHeader>
+                    <ItemDescription className="flex gap-2">
+                      <span className="flex justify-center gap-1">
+                        {e.dungeons}
+                        <Swords></Swords>
+                      </span>
+                      <span className="flex justify-center gap-1">
+                        {e.cards}
+                        <FileStack></FileStack>
+                      </span>
+                    </ItemDescription>
+                  </ItemContent>
+                  <ItemActions>
+                    <Button
+                      onClick={() => {
+                        OnModifyDungeon(e.id);
+                      }}
+                    >
+                      <Pencil></Pencil>
+                    </Button>
+                    <Button
+                      variant={"destructive"}
+                      onClick={() => {
+                        OnWorldDelete(e.id);
+                      }}
+                    >
+                      <Trash></Trash>
+                    </Button>
+                  </ItemActions>
+                </Item>
+                <Separator></Separator>
+              </>
+            );
+          })}
+        </div>
       </section>
 
       <section className="mt-9">
