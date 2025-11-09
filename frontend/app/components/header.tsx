d32ecb4b9781fb8b80e3c83d1412112d90ecd15c
SledDev <sleddev@asd.dog>
2025-11-09 02:28:30 +0100

    i think about you what can i do

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -5,7 +5,7 @@ import {
   Swords,
   WandSparkles,
 } from "lucide-react";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { Link, useNavigate } from "react-router";
 import { MenuToggleIcon } from "~/components/menu-toggle-icon";
@@ -19,11 +19,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
+import { API_URL } from "~/constants";
 import { UserContext } from "~/context/UserContext";
 import { cn } from "~/lib/utils";
 import { Logo } from "./Logo";
-import { API_URL } from "~/constants";
-import type { UserMe } from "~/models";
 
 export function Header() {
   const [open, setOpen] = useState(false);
@@ -33,21 +32,6 @@ export function Header() {
 
   const isPlayer = window.location.pathname.startsWith("/app/player");
   const isMaster = window.location.pathname.startsWith("/app/master");
-
-  const links = [
-    {
-      label: "Features",
-      href: "#",
-    },
-    {
-      label: "Pricing",
-      href: "#",
-    },
-    {
-      label: "About",
-      href: "#",
-    },
-  ];
 
   async function handleLogout() {
     setLogoutLoading(true);
@@ -73,16 +57,6 @@ export function Header() {
           <Logo />
         </Link>
         <div className="hidden items-center gap-2 md:flex">
-          {/* {links.map((link, i) => (
-            <a
-              className={buttonVariants({ variant: "ghost" })}
-              href={link.href}
-              key={i}
-            >
-              {link.label}
-            </a>
-          ))} */}
-
           <div>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -178,25 +152,85 @@ export function Header() {
         </Button>
       </nav>
       <MobileMenu className="flex flex-col justify-between gap-2" open={open}>
-        <div className="grid gap-y-2">
-          {links.map((link) => (
-            <a
-              className={buttonVariants({
-                variant: "ghost",
-                className: "justify-start",
-              })}
-              href={link.href}
-              key={link.label}
-            >
-              {link.label}
-            </a>
-          ))}
-        </div>
+        <div className="grid gap-y-2"></div>
         <div className="flex flex-col gap-2">
-          <Button className="w-full bg-transparent" variant="outline">
-            Sign In
-          </Button>
-          <Button className="w-full">Get Started</Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="ghost"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarFallback className="rounded-lg flex items-center justify-center font-bold w-full h-full bg-linear-to-br from-blue-400 to-violet-500">
+                    <span>{user.display_name.slice(0, 2).toUpperCase()}</span>
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">
+                    {user.display_name}
+                  </span>
+                </div>
+                <ChevronsUpDown className="ml-auto size-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              side="bottom"
+              align="center"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="p-0 font-normal">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarFallback className="rounded-lg flex items-center justify-center font-bold w-full h-full bg-linear-to-br from-blue-400 to-violet-500">
+                      <span>{user.display_name.slice(0, 2).toUpperCase()}</span>
+                    </AvatarFallback>{" "}
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">
+                      {user.display_name}
+                    </span>
+                    <span className="truncate text-muted-foreground text-xs">
+                      @{user.username}
+                    </span>
+                  </div>
+                </div>
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem
+                onClick={handleLogout}
+                className="cursor-pointer"
+              >
+                {logoutLoading ? (
+                  <Loader2 className="animate-spin" />
+                ) : (
+                  <LogOut />
+                )}
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {isMaster && (
+            <Link
+              className={buttonVariants({ variant: "outline" })}
+              to="/app/player"
+            >
+              <Swords />
+              Váltás játékos nézetre
+            </Link>
+          )}
+          {isPlayer && (
+            <Link
+              className={buttonVariants({ variant: "outline" })}
+              to="/app/master"
+            >
+              <WandSparkles />
+              Váltás játékmester nézetre
+            </Link>
+          )}
         </div>
       </MobileMenu>
     </header>
